d576ea1d94e9e3d78df38eea7c53c4bf49c52ef9
shreder0804 <shreder0804@gmail.com>
2021-10-19 09:31:48 +0900

    First Commit

---
+++ server/src/main/java/com/Yana/Buddy/BuddyApplication.java
@@ -0,0 +1,13 @@
+package com.Yana.Buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyApplication.class, args);
+	}
+
+}
