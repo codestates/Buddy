fd7a3932a51dd866a6a47f7350dbbd38e2fb789d
nmin11 <nmin1124@gmail.com>
2021-10-22 13:34:46 +0900

    EDIT : base setting

--- server/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
--- Test Dummy Data
-
---user
-INSERT INTO `user` (user_id ,email, state_message, gender, authority,password,created_at,last_modified_at) VALUES (1,"Kimcoding@code.states","나 살아있다!","MALE","GENERAL",1234,null,null);
-INSERT INTO `user` (user_id, email, state_message, gender, authority,password,created_at,last_modified_at) VALUES (2,"parkhacker@code.states","나 살아있다!","FEMALE","GENERAL",1234,null,null);
-INSERT INTO `user` (user_id, email, state_message, gender, authority,password,created_at,last_modified_at) VALUES (3,"test@code.states","나 살아있다!","FEMALE","GENERAL",2345,null,null);
