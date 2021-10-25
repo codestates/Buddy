f699f701a9d22c7a07c7d36b2850a717828cb9d4
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-10-25 11:09:13 +0900

    EDIT : token_check API

---
+++ server/src/main/java/com/Yana/Buddy/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.Yana.Buddy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+    private String tokenType;
+
+}
