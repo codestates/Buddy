9e09e4c1831a1193c279068c75047f1ba810a88c
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-10-25 17:54:58 +0900

    FIX : CORS error

--- server/src/main/java/com/Yana/Buddy/controller/OAuthLoginController.java
+++ server/src/main/java/com/Yana/Buddy/controller/OAuthLoginController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://yana-buddy.com, http://bucket-yana-buddy.s3-website.ap-northeast-2.amazonaws.com", allowedHeaders = "*", allowCredentials = "true")
 public class OAuthLoginController {
 
     private final UserService userService;
