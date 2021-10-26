8cbf96d6b4eafa52f370e6d7576506a78482ef2c
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-10-26 22:26:29 +0900

    ADD : kakao sign-up logic

---
+++ server/src/main/java/com/Yana/Buddy/dto/KakaoRegisterDto.java
@@ -0,0 +1,13 @@
+package com.Yana.Buddy.dto;
+
+import lombok.Data;
+
+@Data
+public class KakaoRegisterDto {
+
+    private String email;
+    private String nickname;
+    private String profile_image;
+    private String gender;
+
+}
