65953ca414ae67801a5d0c2773483ae367ddd9a6
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-10-26 11:37:05 +0900

    EDIT : google login logic

--- server/src/main/java/com/Yana/Buddy/controller/OAuthLoginController.java
+++ server/src/main/java/com/Yana/Buddy/controller/OAuthLoginController.java
@@ -5,7 +5,6 @@ import com.Yana.Buddy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
