a78147e5dcbacad921cd0d99e88e34c6960998ba
shreder0804 <shreder0804@gmail.com>
2021-10-26 16:57:57 +0900

    UPDATE : Kakao Oauth

--- client/src/components/modals/LoginModal.js
+++ client/src/components/modals/LoginModal.js
@@ -68,12 +68,6 @@ export function LoginModal(props) {
       })
         .then((res) => {
           console.log(res.data);
-          setUserInfo(res.data); // res.data userInfo에 저장
-          console.log(cookies.get('refreshToken'));
-          cookies.set('refreshToken', res.data.refreshToken);
-          props.setLoginOn(true); // 로그인 true
-          history.push('/');
-          accessTokenCheck(); // 새로고침 시 로그인 유지
         })
         .catch((err) => {});
     }
