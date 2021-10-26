5a466514b145d771e36692fd0f03294506c904c7
shreder0804 <shreder0804@gmail.com>
2021-10-26 20:06:33 +0900

    aa

--- client/src/components/modals/LoginModal.js
+++ client/src/components/modals/LoginModal.js
@@ -94,13 +94,15 @@ export function LoginModal(props) {
             .then((res) => {
               console.log(res.data.access_token);
 
+              let copyUserInfo = { ...userInfo };
+
+              copyUserInfo.email = `${res.data.kakao_account.email}`;
+              copyUserInfo.gender = `${res.data.kakao_account.gender}`;
+              copyUserInfo.nickname = `${res.data.properties.nickname}`;
+              copyUserInfo.authority = 'GENERAL';
+              setUserInfo(copyUserInfo);
               console.log(cookies.get('kakaoAccessToken'));
-              setUserInfo({
-                email: `${res.data.kakao_account.email}`,
-                gender: `${res.data.kakao_account.gender}`,
-                nickname: `${res.data.properties.nickname}`,
-                authority: 'GENERAL',
-              });
+
               props.setLoginOn(true); // 로그인 true
               history.push('/');
               kakaoAccessTokenCheck(); // 새로고침 시 로그인 유지
@@ -224,13 +226,14 @@ export function LoginModal(props) {
         })
           .then((res) => {
             console.log(res.data);
+            let copyUserInfo = { ...userInfo };
 
-            setUserInfo({
-              email: `${res.data.kakao_account.email}`,
-              gender: `${res.data.kakao_account.gender}`,
-              nickname: `${res.data.properties.nickname}`,
-              authority: 'GENERAL',
-            });
+            copyUserInfo.email = `${res.data.kakao_account.email}`;
+            copyUserInfo.gender = `${res.data.kakao_account.gender}`;
+            copyUserInfo.nickname = `${res.data.properties.nickname}`;
+            copyUserInfo.authority = 'GENERAL';
+
+            setUserInfo(copyUserInfo);
             console.log(setUserInfo);
 
             props.setLoginOn(true); // 로그인 true
