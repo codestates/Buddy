763091b0f9827511dc101843eaa4c05bb6938c92
nmin11 <nmin1124@gmail.com>
2021-10-29 10:26:24 +0900

    ADD : email confirm logic

--- server/src/main/java/com/Yana/Buddy/controller/EmailController.java
+++ server/src/main/java/com/Yana/Buddy/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.Yana.Buddy.controller;
 import com.Yana.Buddy.dto.CodeDto;
 import com.Yana.Buddy.dto.EmailDto;
 import com.Yana.Buddy.service.EmailService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
+@Log4j2
 @RestController
 public class EmailController {
 
@@ -23,35 +25,33 @@ public class EmailController {
     }
 
     @PostMapping("/email_confirm")
-    public ResponseEntity<?> emailConfirm(@RequestBody (required = true) EmailDto email) throws Exception {
-        try{
+    public ResponseEntity<?> emailConfirm(@RequestBody EmailDto email) throws Exception {
+        try {
             inputCode = emailService.sendSimpleMessage(email.getEmail());
             return ResponseEntity.status(200).body(new HashMap<>(){
                 {
-                    put("message","ok");
+                    put("message", "ok");
                 }
             });
-        }catch (NullPointerException e){
-            System.out.println(e);
+        } catch (NullPointerException e){
+            log.error("Null Pointer Exception : " + e);
             return null;
         }
 
     }
 
     @PostMapping("/email_code_check")
-    public ResponseEntity<?> emailCodeCheck(@RequestBody(required = true) CodeDto code){
-        if(inputCode.equals(code.getCode())){
+    public ResponseEntity<?> emailCodeCheck(@RequestBody CodeDto code){
+        if (inputCode.equals(code.getCode())){
             return ResponseEntity.status(200).body(new HashMap<>(){
                 {
-                    put("message","ok");
+                    put("message", "ok");
                 }
             });
-        }else{
-            System.out.println(code);
-            System.out.println(inputCode);
+        } else {
             return ResponseEntity.status(400).body(new HashMap<>(){
                 {
-                    put("message","인증번호가 틀렸습니다");
+                    put("message", "인증번호가 틀렸습니다");
                 }
             });
         }
