b16ae8426faa513e4456d83b5ace92631dad5316
shreder0804 <shreder0804@gmail.com>
2021-10-29 11:02:56 +0900

    ADD : Signup Email Code Check

--- client/src/components/modals/SignupModal.js
+++ client/src/components/modals/SignupModal.js
@@ -6,14 +6,16 @@ import dotenv from 'dotenv';
 import { PASSWORD_REGEXP, EMAIL_REGEXP } from '../../constants/constants';
 
 export function SignupModal(props) {
-  const [signupUserEmail, setSignupUserEmail] = useState('');
-  const [signupUserEmailCheck, setSignupUserEmailCheck] = useState(0);
-  const [signupUserPassword, setSignupUserPassword] = useState('');
-  const [signupUserPasswordValid, setSignupUserPasswordValid] = useState('');
-  const [signupUserNickname, setSignupUserNickname] = useState('');
-  const [signupUserNicknameCheck, setSignupUserNicknameCheck] = useState(0);
-  const [signupUserGender, setSignupUserGender] = useState('MALE');
-  const [signupUserinfo, setSignupUserinfo] = useState({});
+  const [signupUserEmail, setSignupUserEmail] = useState(''); // 이메일
+  const [signupUserEmailCheck, setSignupUserEmailCheck] = useState(0); // 이메일 중복 체크
+  const [signupUserEmailCode, setSignupUserEmailCode] = useState(''); // 이메일 인증 코드
+  const [signupUserEmailCodeCheck, setSignupUserEmailCodeCheck] = useState(0); // 이메일 인증 코드
+  const [signupUserPassword, setSignupUserPassword] = useState(''); // 비밀번호
+  const [signupUserPasswordValid, setSignupUserPasswordValid] = useState(''); // 비밀번호 재입력
+  const [signupUserNickname, setSignupUserNickname] = useState(''); // 닉네임
+  const [signupUserNicknameCheck, setSignupUserNicknameCheck] = useState(0); // 닉네임 중복 체크
+  const [signupUserGender, setSignupUserGender] = useState('MALE'); // 성별
+  const [signupUserinfo, setSignupUserinfo] = useState({}); // 유저 정보 객체
 
   const history = useHistory();
 
@@ -22,6 +24,7 @@ export function SignupModal(props) {
     if (
       signupUserEmailCheck === 1 &&
       signupUserNicknameCheck === 1 &&
+      signupUserEmailCodeCheck === 1 &&
       PASSWORD_REGEXP.test(signupUserPassword) &&
       signupUserPassword === signupUserPasswordValid &&
       signupUserPasswordValid !== ''
@@ -41,10 +44,13 @@ export function SignupModal(props) {
           console.log(res.data);
           props.setSignupModalOn(false);
           setSignupUserEmail('');
+          setSignupUserEmailCheck(0);
+          setSignupUserEmailCode('');
+          setSignupUserEmailCodeCheck(0);
           setSignupUserPassword('');
           setSignupUserPasswordValid('');
           setSignupUserNickname('');
-          setSignupUserGender('MALE');
+          setSignupUserNicknameCheck(0);
           history.push('/');
         })
         .catch((err) => {
@@ -60,9 +66,13 @@ export function SignupModal(props) {
     } else {
       props.setSignupModalOn(false);
       setSignupUserEmail('');
+      setSignupUserEmailCheck(0);
+      setSignupUserEmailCode('');
+      setSignupUserEmailCodeCheck(0);
       setSignupUserPassword('');
       setSignupUserPasswordValid('');
       setSignupUserNickname('');
+      setSignupUserNicknameCheck(0);
       setSignupUserGender('MALE');
     }
   };
@@ -74,11 +84,65 @@ export function SignupModal(props) {
     console.log(signupUserEmail);
   };
 
+  // 이메일 인증코드 입력 상태관리
+  const handleChangeEmailCode = (e) => {
+    setSignupUserEmailCode(e.target.value);
+    setSignupUserEmailCodeCheck(0);
+    console.log(signupUserEmailCode);
+  };
+
   // 이메일 중복 체크
   const handleEmailValidCheck = () => {
-    axios(`${process.env.REACT_APP_API_URL}/email_check`, {
+    if (EMAIL_REGEXP.test(signupUserEmail)) {
+      axios(`${process.env.REACT_APP_API_URL}/email_check`, {
+        method: 'POST',
+        data: { email: signupUserEmail },
+        headers: {
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Credentials': 'true',
+        },
+        withCredentials: true,
+      })
+        .then((res) => {
+          console.log(res.data);
+          setSignupUserEmailCheck(1);
+          console.log('사용 가능한 이메일입니다.');
+
+          // 이메일 인증 코드 보내기
+          axios(`${process.env.REACT_APP_API_URL}/email_confirm`, {
+            method: 'POST',
+            data: { email: signupUserEmail },
+            headers: {
+              'Access-Control-Allow-Headers': 'Content-Type',
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'POST',
+              'Access-Control-Allow-Credentials': 'true',
+            },
+            withCredentials: true,
+          })
+            .then((res) => {
+              console.log(res.data);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        })
+        .catch((err) => {
+          console.error(err);
+          setSignupUserEmailCheck(2);
+          console.log('이미 존재하는 이메일입니다!');
+        });
+    }
+  };
+
+  // 이메일 인증코드 체크
+  const handleEmailCodeCheck = () => {
+    // 이메일 코드 일치 확인
+    axios(`${process.env.REACT_APP_API_URL}/email_code_check`, {
       method: 'POST',
-      data: { email: signupUserEmail },
+      data: { code: signupUserEmailCode },
       headers: {
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Origin': '*',
@@ -89,17 +153,15 @@ export function SignupModal(props) {
     })
       .then((res) => {
         console.log(res.data);
-
-        setSignupUserEmailCheck(1);
-        console.log('사용 가능한 이메일입니다.');
+        setSignupUserEmailCodeCheck(1);
+        console.log('이메일 코드가 일치합니다.');
       })
       .catch((err) => {
         console.error(err);
-        setSignupUserEmailCheck(2);
-        console.log('이미 존재하는 이메일입니다!');
+        setSignupUserEmailCodeCheck(2);
+        console.log('이메일 코드가 일치하지 않습니다.');
       });
   };
-
   // 비밀번호 입력 상태관리
   const handleChangePassword = (e) => {
     setSignupUserPassword(e.target.value);
@@ -196,12 +258,39 @@ export function SignupModal(props) {
                 {!EMAIL_REGEXP.test(signupUserEmail) && signupUserEmail !== '' ? (
                   <span className="signup__error__message">이메일 주소에 @를 포함해주세요.</span>
                 ) : (!EMAIL_REGEXP.test(signupUserEmail) && signupUserEmail !== '') || signupUserEmailCheck === 1 ? (
-                  <span className="signup__correct">사용 가능한 이메일입니다.</span>
+                  <span className="signup__correct">
+                    이메일로 인증번호가 전송되었습니다. <br /> 인증번호를 입력해주세요.
+                  </span>
                 ) : (!EMAIL_REGEXP.test(signupUserEmail) && signupUserEmail !== '') || signupUserEmailCheck === 2 ? (
                   <span className="signup__error__message">중복된 이메일입니다.</span>
                 ) : (
                   <span className="signup__error__message"></span>
                 )}
+              </div>
+              {signupUserEmailCheck === 1 ? (
+                <div className="signup__emailcheck__wrappers">
+                  <fieldset className="signup__emailcheck__container">
+                    <input
+                      className="signup__input"
+                      onChange={handleChangeEmailCode}
+                      type="text"
+                      placeholder="인증번호 입력"
+                      maxLength="10"
+                      value={signupUserEmailCode}
+                      autocomplete="false"
+                    ></input>
+                  </fieldset>
+                  <button className="signup__emailcheck__btn" onClick={handleEmailCodeCheck}>
+                    확인
+                  </button>
+                </div>
+              ) : null}
+              <div className="signup__error">
+                {signupUserEmailCodeCheck === 1 ? (
+                  <span className="signup__correct">이메일 인증코드가 일치합니다.</span>
+                ) : signupUserEmailCodeCheck === 2 ? (
+                  <span className="signup__error__message">이메일 인증코드가 일치하지 않습니다.</span>
+                ) : null}
               </div>
               <div className="signup__input__wrappers">
                 <fieldset className="signup__input__container">
