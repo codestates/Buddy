26059733e5972f1f0e4cc07c08c86711639510c0
nmin11 <nmin1124@gmail.com>
2021-11-03 09:56:58 +0900

    ADD : test file

--- server/src/main/java/com/Yana/Buddy/handler/StompHandler.java
+++ server/src/main/java/com/Yana/Buddy/handler/StompHandler.java
@@ -1,10 +1,10 @@
 package com.Yana.Buddy.handler;
 
 import com.Yana.Buddy.entity.ChatMessage;
+import com.Yana.Buddy.repository.ChatRoomRepository;
 import com.Yana.Buddy.service.ChatMessageService;
 import com.Yana.Buddy.service.ChatRoomService;
 import com.Yana.Buddy.service.TokenService;
-import com.Yana.Buddy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.Message;
@@ -14,6 +14,7 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Log4j2
@@ -22,36 +23,37 @@ import java.util.Optional;
 public class StompHandler implements ChannelInterceptor {
 
     private final TokenService tokenService;
-    private final UserService userService;
-    private final ChatRoomService chatRoomService;
+    private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageService chatMessageService;
+    private final ChatRoomService chatRoomService;
 
+    //WebSocket 을 통해 들어온 요청이 처리 되기전 실행
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
+        // WebSocket 연결시 헤더의 jwt token 검증
         if (StompCommand.CONNECT == accessor.getCommand()) {
-            String token = accessor.getFirstNativeHeader("token");
-            log.info("CONNECT -> JWT TOKEN : {}", token);
+            String jwtToken = accessor.getFirstNativeHeader("token");
+            log.info("CONNECT : {}", jwtToken);
         }
 
+        /**
+         1. header 정보에서 구독 destination 정보를 얻고 roomId 추출
+         2. 채팅방에 들어온 클라이언트 sessionId를 roomId와 매핑 (추후 특정 세션이 어느 방에 들어가 있는지 확인하는 용도)
+         3. 채팅방 인원++
+         4. 클라이언트 입장 메시지를 채팅방에 발송 (redis publish)
+         */
         else if (StompCommand.SUBSCRIBE == accessor.getCommand()) {
-            /*
-            header 정보에서 구독 destination 정보를 얻고 roomId 추출
-            roomId는 URL 통해서 전송되고 있음
-             */
-            String roomId = chatMessageService.getRoomId(
-                    Optional.ofNullable((String) message.getHeaders()
-                            .get("simpDestintaion")).orElse("InvalidRoomId"));
+            String roomId = chatMessageService.getRoomId(Optional.ofNullable(
+                    (String) message.getHeaders().get("simpDestination"))
+                    .orElse("Anonymous User"));
 
-
-            //채팅방에 들어온 클라이언트의 sessionId를 roomId와 매핑(추후 세션이 어떤 채팅방에 있는지 확인하기 위함)
             String sessionId = (String) message.getHeaders().get("simpSessionId");
-            chatRoomService.setEnterUserInfo(sessionId, roomId);
+            chatRoomService.setUserEnterInfo(sessionId, roomId);
 
-            //클라이언트 입장 메시지를 채팅방에 발송 (redis push)
-            String token = Optional.ofNullable(accessor.getFirstNativeHeader("token")).orElse("UnknownUser");
-            String name = userService.findUserByEmail(tokenService.checkJwtToken(token).get("email")).getNickname();
+            String name = Optional.ofNullable((Principal) message.getHeaders().get("simpUser"))
+                    .map(Principal::getName).orElse("Anonymous User");
             chatMessageService.sendChatMessage(ChatMessage.builder()
                             .type(ChatMessage.MessageType.ENTER)
                             .roomId(roomId)
@@ -61,25 +63,25 @@ public class StompHandler implements ChannelInterceptor {
             log.info("SUBSCRIBED {}, {}", name, roomId);
         }
 
+        /**
+         1. 연결이 종료된 클라이언트 sessionId를 통해서 채팅방 id 획득
+         2. 채팅방 인원--
+         3. 클라이언트 퇴장 메시지를 채팅방에 발송 (redis publish)
+         */
         else if (StompCommand.DISCONNECT == accessor.getCommand()) {
-            //연결 종료된 클라이언트 sessionId 를 통해 채팅방 id 얻기
             String sessionId = (String) message.getHeaders().get("simpSessionId");
-            String roomId = chatRoomService.getEnterUserId(sessionId);
+            String roomId = chatRoomService.getUserEnterRoomId(sessionId);
 
-            //클라이언트 퇴장 메시지를 채팅창에 발송 (redis publish)
-            String token = Optional.ofNullable(accessor.getFirstNativeHeader("token")).orElse("UnknownUser");
+            String name = Optional.ofNullable((Principal) message.getHeaders().get("simpUser"))
+                    .map(Principal::getName).orElse("Anonymous User");
+            chatMessageService.sendChatMessage(ChatMessage.builder()
+                            .type(ChatMessage.MessageType.QUIT)
+                            .roomId(roomId)
+                            .sender(name)
+                            .build());
+            chatRoomService.removeUserEnterInfo(sessionId);
 
-            if (accessor.getFirstNativeHeader("token") != null) {
-                String name = userService.findUserByEmail(tokenService.checkJwtToken(token).get("email")).getNickname();
-                chatMessageService.sendChatMessage(ChatMessage.builder()
-                        .type(ChatMessage.MessageType.QUIT)
-                        .roomId(roomId)
-                        .sender(name)
-                        .build());
-            }
-
-            chatRoomService.removeEnterUserInfo(sessionId);
-            log.info("DISCONNECT {}, {}", sessionId, roomId);
+            log.info("DISCONNECTED {}, {}", sessionId, roomId);
         }
 
         return message;
--- server/src/main/java/com/Yana/Buddy/service/ChatMessageService.java
+++ server/src/main/java/com/Yana/Buddy/service/ChatMessageService.java
@@ -2,14 +2,12 @@ package com.Yana.Buddy.service;
 
 import com.Yana.Buddy.entity.ChatMessage;
 import com.Yana.Buddy.repository.ChatMessageRepository;
-import com.Yana.Buddy.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,46 +16,35 @@ public class ChatMessageService {
     private final ChannelTopic channelTopic;
     private final RedisTemplate redisTemplate;
     private final ChatMessageRepository chatMessageRepository;
-    private final ChatRoomRepository chatRoomRepository;
-    private final UserService userService;
 
     //destination 정보에서 roomId 추출
     public String getRoomId(String destination) {
         int lastIndex = destination.lastIndexOf('/');
         if (lastIndex != -1) {
             return destination.substring(lastIndex + 1);
-        }
-        else return "";
+        } else return "";
     }
 
-    //채팅방에 메시지 발송
-    public void sendChatMessage(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-            message.setMessage(message.getSender() + "님이 입장했습니다.");
-            message.setSender("[알림]");
-        } else if (ChatMessage.MessageType.QUIT.equals(message.getType())) {
-            message.setMessage(message.getSender() + "님이 퇴장했습니다.");
-            message.setSender("[알림]");
-        }
-        redisTemplate.convertAndSend(channelTopic.getTopic(), message);
+    public List<ChatMessage> getAllMessage(Long roomId) {
+        return chatMessageRepository.findByRoom(roomId);
     }
 
     public void save(ChatMessage message) {
-        ChatMessage msg = new ChatMessage();
-        msg.setType(message.getType());
-        msg.setRoomId(msg.getRoomId());
-        msg.setUser(userService.findUserById(msg.getChatUserId()));
-        msg.setChatUserId(msg.getChatUserId());
-        msg.setSender(message.getSender());
-        msg.setMessage(message.getMessage());
-        msg.setCreatedAt(message.getCreatedAt());
         chatMessageRepository.save(message);
     }
 
-    public Page<ChatMessage> getChatMessageByRoomId(String roomId, Pageable pageable) {
-        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
-        pageable = PageRequest.of(page, 150);
-        return chatMessageRepository.findByRoomId(roomId, pageable);
+    public void sendChatMessage(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender() + " 님이 입장했어요! 🤩");
+            message.setSender("[알림]");
+        }
+
+        else if (ChatMessage.MessageType.QUIT.equals(message.getType())) {
+            message.setMessage(message.getSender() + " 님이 퇴장했어요... 🥺");
+            message.setSender("[알림]");
+        }
+
+        redisTemplate.convertAndSend(channelTopic.getTopic(), message);
     }
 
 }
