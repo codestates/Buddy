13e703d06be9fd3c7383f35cbe282d82c0219553
suyeon <namtndus@gmail.com>
2021-11-04 10:28:06 +0900

    EEIDTE: prohibiedUser saving create

--- server/src/main/java/com/Yana/Buddy/repository/ProhibiedUserRepository.java
+++ server/src/main/java/com/Yana/Buddy/repository/ProhibiedUserRepository.java
@@ -1,2 +1,7 @@
-package com.Yana.Buddy.repository;public interface ProhibiedUserRepository {
+package com.Yana.Buddy.repository;
+
+import com.Yana.Buddy.entity.ProhibiedUserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProhibiedUserRepository extends JpaRepository<ProhibiedUserEntity, Long> {
 }
