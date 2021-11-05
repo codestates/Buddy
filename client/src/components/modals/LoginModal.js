09e5c96a219d1e2c559ccf3739f985c35419187f
shreder0804 <shreder0804@gmail.com>
2021-11-05 22:57:16 +0900

    Fix repo

--- client/src/components/modals/LoginModal.js
+++ client/src/components/modals/LoginModal.js
@@ -62,7 +62,7 @@ export function LoginModal(props) {
     if (kakaoSearch) {
       const kakaoCode = kakaoSearch.split('=')[1].split('&')[0]; // google code 값만 추출
 
-      axios(`${process.env.REACT_APP_LOCAL_URL}/oauth/kakao/callback?code=${kakaoCode}`, {
+      axios(`${process.env.REACT_APP_API_URL}/oauth/kakao/callback?code=${kakaoCode}`, {
         method: 'GET',
       })
         .then((res) => {
