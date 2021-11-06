7f0570a886d1b6d1d1dd0ebc4b0585d30789aa9e
nmin11 <nmin1124@gmail.com>
2021-11-06 15:26:33 +0900

    ADD : delete room request

--- server/src/main/java/com/Yana/Buddy/repository/ChatRoomRepository.java
+++ server/src/main/java/com/Yana/Buddy/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.Yana.Buddy.repository;
 
 import com.Yana.Buddy.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     ChatRoom findByRoomId(String roomId);
     List<ChatRoom> findByUserCount(int count);
+    void deleteByRoomId(String roomId);
 
 }
