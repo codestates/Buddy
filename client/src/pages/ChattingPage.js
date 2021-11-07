0bc606244d7bf42e1ed7c4871b780b40c31d8185
shreder0804 <shreder0804@gmail.com>
2021-11-07 15:22:47 +0900

    Fix repo

--- client/src/pages/ChattingPage.js
+++ client/src/pages/ChattingPage.js
@@ -44,7 +44,7 @@ export function ChattingPage(props) {
   const token = cookies.get('refreshToken');
 
   // 소켓 통신 객체
-  const sock = new SockJS(`${process.env.REACT_APP_LOCAL_URL}/chatting`);
+  const sock = new SockJS(`${process.env.REACT_APP_API_URL}/chatting`);
   const ws = Stomp.over(sock);
 
   useEffect(() => {
@@ -68,7 +68,7 @@ export function ChattingPage(props) {
 
   // 방 만들기(대기 중인 방 찾기)
   const handleCreateRoom = () => {
-    axios(`${process.env.REACT_APP_LOCAL_URL}/chat/room`, {
+    axios(`${process.env.REACT_APP_API_URL}/chat/room`, {
       method: 'GET',
       headers: AXIOS_DEFAULT_HEADER,
     })
