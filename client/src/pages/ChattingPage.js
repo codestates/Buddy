7c12c6cdebb56e58413005ecb7b1f0e83dbbfa02
shreder0804 <shreder0804@gmail.com>
2021-11-08 10:32:52 +0900

    UPDATE : HTTPS URL

--- client/src/pages/ChattingPage.js
+++ client/src/pages/ChattingPage.js
@@ -34,7 +34,7 @@ export function ChattingPage(props) {
   const cookies = new Cookies();
 
   // 소켓 통신 객체
-  const sock = new SockJS(`${process.env.REACT_APP_LOCAL_URL}/chatting`);
+  const sock = new SockJS(`${process.env.REACT_APP_HTTPS_URL}/chatting`);
   const ws = Stomp.over(sock);
 
   // 토큰
@@ -60,7 +60,7 @@ export function ChattingPage(props) {
 
   // 방 만들기(대기 중인 방 찾기)
   const handleCreateRoom = () => {
-    axios(`${process.env.REACT_APP_LOCAL_URL}/chat/room`, {
+    axios(`${process.env.REACT_APP_HTTPS_URL}/chat/room`, {
       method: 'GET',
       headers: AXIOS_DEFAULT_HEADER,
     })
